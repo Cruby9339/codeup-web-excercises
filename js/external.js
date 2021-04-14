ace75c91f15697c08641bac3c8f3c4ff75cd2ad4
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-04-14 13:12:11 -0500

    Js practice

---
+++ js/external.js
@@ -0,0 +1,14 @@
+"use strict";
+
+alert("Welcome to my website!");
+
+let userColor = prompt("What is your favorite color?");
+
+userColor === "green" ? alert("My favorite color is also " + userColor) : alert("Your favorite color is " + userColor);
+
+
+// if(userColor === "green"){
+//     alert("My favorite color is also " + userColor);
+// } else{
+//     alert("Your favorite color is " + userColor);
+// }

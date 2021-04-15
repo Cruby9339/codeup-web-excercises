76c491f790b1c81598da9c3ed091cf29291a8a56
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-04-15 16:07:29 -0500

    Shorthand Unary Operator Practice

---
+++ js/shorthand-unary-and-js-methods.js
@@ -0,0 +1,55 @@
+"use strict";
+
+//Shorthand Assignment Operators
+
+// += / -=
+
+var name = "Christian";
+
+// name = name + " Michael";
+// console.log(name);
+
+// name += (" " + "Michael");
+// console.log(name);
+//
+// // console.log(name -= "Michael");
+//
+// var daysRemainingInCohort = 114;
+//
+// daysRemainingInCohort -= 4;
+//
+// console.log(daysRemainingInCohort);
+
+// Mini Exercise
+
+var result = 1;
+
+result *= 4;
+
+console.log(result);
+
+var resultTwo = 2;
+
+resultTwo += 2;
+
+console.log(resultTwo);
+
+var resultThree = 20;
+
+resultThree /= 5;
+
+console.log(resultThree);
+
+var resultFour = 4;
+
+resultFour %= 15;
+
+console.log(resultFour);
+
+var resultFive = 6;
+
+resultFive -= 2;
+
+console.log(resultFive);
+
+

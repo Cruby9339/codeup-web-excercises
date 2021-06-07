7b52554b6f67b5347c30fb5f8d3bfe5f33532bd3
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-06-06 22:37:53 -0500

    adds jquery traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -12,6 +12,40 @@ $(document)
 	.ready(function () {
 		$('dt')
 			.click(function () {
-			$(this).css('background-color', 'yellow')
+				$(this)
+					.css('background-color', 'yellow')
+			})
+	})
+
+$(document)
+	.ready(function () {
+		$('#highlight-li')
+			.click(function () {
+				$('ul li:last-child')
+					.css('background-color', 'yellow')
+			})
+	})
+
+
+$(document)
+	.ready(function () {
+		$('h3')
+			.click(function () {
+				$(this)
+					.next()
+					.children()
+					.css('font-weight', 'bold')
+			})
+	})
+
+$(document)
+	.ready(function () {
+		$('li')
+			.click(function () {
+				$(this)
+					.parent()
+					.children()
+					.first()
+					.css('color', 'blue')
 			})
 	})
